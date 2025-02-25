272138fc93f943addd2b0bcabcad5d29ad8e7f43
Heorhii-13 <heorhii.batyrov@gmail.com>
2025-02-25 09:41:50 +0200

    first

---
+++ Simple_calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+abstract class Simple_calculator
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("choose operator(+, -, *, /): ");
+        char op;
+        op = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Enter the second nummber: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                    Console.WriteLine("It cannot be divided by 0!");
+                break;
+            default:
+                Console.WriteLine("Invalid operator!");
+                return;
+        }
+
+        Console.WriteLine($"Calculation result: {result}");
+    }
+}
